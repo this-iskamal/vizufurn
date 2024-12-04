8d4bde36288ee199115340fcc8b0e5c4a87aaff4
this-iskamal <kamal.gautam.36808@gmail.com>
2024-12-04 23:46:07 +0545

    test to host to azure vm

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -1,12 +1,23 @@
 import { Product } from "../models/index.js";
 
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find().exec();
+    return res.status(200).send(products);
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
+};
+
+
 export const getProductByCategoryId = async (req, res) => {
   const { categoryId } = req.params;
 
   try {
-    const  products = await Product.find({category: categoryId})
-    .select("-category")
-    .exec();
+    const products = await Product.find({ category: categoryId })
+      .select("-category")
+      .exec();
     return res.status(200).send(products);
   } catch (error) {
     return res.status(500).send({ message: error.message });
--- api/routes/product.routes.js
+++ api/routes/product.routes.js
@@ -1,5 +1,6 @@
-import { getProductByCategoryId } from "../controllers/product.controller.js";
+import { getAllProducts, getProductByCategoryId } from "../controllers/product.controller.js";
 
 export const productRoutes = async (fastify,options)=>{
     fastify.get("/products/:categoryId", getProductByCategoryId);
+    fastify.get("/products", getAllProducts);
 }
